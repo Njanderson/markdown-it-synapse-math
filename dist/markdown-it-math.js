52186c0c84f89708dfa45b14fb7a7d8651c6508d
Jay Hodgson <jay.hodgson@sagebase.org>
2016-04-12 16:04:05 -0700

    SWC-2969: alternate syntax for inline math (single $ marker) also needs to be supported

--- dist/markdown-it-math.js
+++ dist/markdown-it-math.js
@@ -1,4 +1,4 @@
-/*! markdown-it-math 3.0.2 https://github.com/runarberg/markdown-it-math @license MIT */
+/*! markdown-it-math 3.0.3 https://github.com/runarberg/markdown-it-math @license MIT */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownitMath = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 var prefix = 'mathjax-';
@@ -9,9 +9,16 @@ function inlineParser(state, silent) {
       found = false,
       max = state.posMax,
       start = state.pos,
-      open = '$$',
+      ch  = state.src.charCodeAt(start),
+      open = '$',
+      close = '$';
+  if (ch !== 0x24/* $ */ || start >= max) { return false; }
+  if (state.pos + 1 < state.posMax) {
+    if (state.src.charCodeAt(start + 1) === 0x24 /* $ */) {
+      open = '$$';
       close = '$$';
-
+    }
+  }
   if (state.src.slice(state.pos, state.pos + open.length) !== open) {
     return false;
   }
